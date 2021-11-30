fbb639814b3082fbc1cbe6f939bf6b74276d70d5
xdjx3runx <xdjx3runx@gmail.com>
2021-11-29 22:43:55 -0800

    Can search spells.2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-$.ajax({
-    url: "https://www.dnd5eapi.co/api/spells/acid-arrow",
-    success: function (spell) {
-      console.log(spell);
-    },
-    error: function (error) {
-      console.log("bad request: ", error);
-    },
-  });
+// const promise = $.ajax({
+//     url: "https://www.dnd5eapi.co/api/spells/acid-arrow",
+// });
+    
+// promise.then(
+//   (data) => {
+//     $('#name').html(data.name);
+//     $('#range').html(data.range);
+//     $('#damage').html(data['damage']['damage_at_slot_level']['2']);
+//   },
+//   (error) => {
+//     console.log("bad request: ", error);
+//   }
+// );
+
+$('form').on('submit', (event) => {
+  event.preventDefault();
+  let userInput = $('input[type="text"]').val();
+    userInput=userInput.replace(' ', '-');
+    $.ajax({
+      url: `https://www.dnd5eapi.co/api/spells/${userInput}`
+    }).then(
+      (data) => {
+        $('#name').html(data.name);
+        $('#range').html(data.range);
+        $('#damage').html(data['damage']['damage_at_slot_level']['2']);
+      },
+      (error) => {
+        console.log("bad request: ", error);
+      }
+    );
+})
